7317da3cb13bda25661a390cff6f198ab146ca7b
Kaviarasan44 <kaviarasan4496@gmail.com>
2023-08-25 19:15:09 +0530

    change made

---
+++ src/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <div className='header'>
+        Erode Amman Mess <sub>Veg/Non-veg</sub>
+      </div>
+      <div className='navbar'>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">About us</a></li>
+            <li><a href="">Gallery</a></li>
+            <li><a href="">Contact us</a></li>
+            <li><a href="">Order Online</a></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Header
